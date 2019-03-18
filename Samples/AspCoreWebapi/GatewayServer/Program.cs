0d731be34dd46214f979a4d2abe8c8fa8552c9bc
IKende <henryfan@msn.com>
2019-03-18 21:45:25 +0800

    add asp.net core web api sample

---
+++ Samples/AspCoreWebapi/GatewayServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GatewayServer
+{
+    class Program
+    {
+        private static Bumblebee.Gateway mGateway;
+
+        static void Main(string[] args)
+        {
+            mGateway = new Bumblebee.Gateway();
+            mGateway.HttpOptions(o => { o.LogToConsole = true; o.Port = 80; o.LogLevel = BeetleX.EventArgs.LogType.Error; });
+            //添中管理的服务，网关会对应用服务的可用状况进行监控
+            mGateway.SetServer("http://192.168.2.18:8001");
+            mGateway.SetServer("http://192.168.2.18:8002");
+            mGateway.SetServer("http://192.168.2.18:8003");
+
+            //default的是'*',匹配优先级最低
+            mGateway.Routes.Default
+               .AddServer("http://192.168.2.18:8001", 10, 100)
+               .AddServer("http://192.168.2.18:8002", 10, 100)
+               .AddServer("http://192.168.2.18:8003", 10, 100);
+            //以上是手动代码的方式来构建网关，实际上可以通过配置'Gateway.json'来描述即可,具本查看https://github.com/IKende/Bumblebee
+            mGateway.Open();
+            Console.Read();
+        }
+    }
+}
---
+++ Samples/AspCoreWebapi/WebServer2/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { $"{this.HttpContext.Connection.LocalIpAddress}${this.HttpContext.Connection.LocalPort}|{DateTime.Now}" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"{this.HttpContext.Connection.LocalIpAddress}${this.HttpContext.Connection.LocalPort}|{DateTime.Now}";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return $"{this.HttpContext.Connection.LocalIpAddress}${this.HttpContext.Connection.LocalPort}|{DateTime.Now}";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody] string value)
+        {
+            return $"{this.HttpContext.Connection.LocalIpAddress}${this.HttpContext.Connection.LocalPort}|{DateTime.Now}";
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            return $"{this.HttpContext.Connection.LocalIpAddress}${this.HttpContext.Connection.LocalPort}|{DateTime.Now}";
+        }
+    }
+}
